a740d13ba460edb77dcb51951fbf8f399ee8c4a6
Vinicius Lima <vinislimadasilva17@gmail.com>
2021-07-08 20:33:57 -0300

    MainView Fields

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,31 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+	try {
+	 FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+	 ScrollPane scrollPane = loader.load();
+	 scrollPane.setFitToHeight(true);
+	 scrollPane.setFitToWidth(true);
+	 Scene mainScene = new Scene(scrollPane);
+	 primaryStage.setScene(mainScene);
+	 primaryStage.setTitle("Sample JavaFX application");
+	 primaryStage.show();
+	 } catch (IOException e) {
+	 e.printStackTrace();
+	 }
+	} 
+
 	
 	public static void main(String[] args) {
 		launch(args);
