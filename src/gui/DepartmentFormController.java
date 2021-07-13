4bf5a4b46141b642598ce0db21ab7a6c5cead545
Vinicius Lima <vinislimadasilva17@gmail.com>
2021-07-13 10:19:39 -0300

    DepartmentForm view

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,8 +9,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable {
+	
+	private Department entity;
 
 	@FXML
 	private TextField txtId;
@@ -27,6 +30,10 @@ public class DepartmentFormController implements Initializable {
 	@FXML
 	private Button btCancel;
 	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+	
 	@FXML
 	public void onBtSaveAction() {
 	System.out.println("oi");	
@@ -39,12 +46,22 @@ public class DepartmentFormController implements Initializable {
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		
+		initializeNodes();
 	}
 	
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("The department is null!");
+		}else {
+			txtId.setText(String.valueOf(entity.getId()));
+			txtName.setText(entity.getName());	
+		}
+		
 	}
 
 }
